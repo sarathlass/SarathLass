6872a657f207dae488c5b63681a4caa0e4ead314
sarathlass <sarathninja1995@gmail.com>
2024-06-02 21:19:35 +0530

    java printing code

---
+++ Github java code.java
@@ -0,0 +1,23 @@
+public class Sarath
+  {
+
+    String name ="sarath";
+    int age =30;
+    
+  public static void main(String[]args)
+
+    {
+
+system.out.println(name);
+      system.out.println(age);
+
+
+      
+    }
+
+
+
+
+
+
+  }
